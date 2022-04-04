fb20dc1c2750a3e0df1141225a4e1bf7427dd285
Ralph Liton <rj_liton@hotmail.com>
2022-04-04 19:59:37 -0400

    added pages

--- js/routing.js
+++ js/routing.js
@@ -1,13 +1,18 @@
-var app = angular.module('myApp', ['ngRoute']); 
-app.config(function($routeProvider) { 
-    $routeProvider  
-    .when('/', { templateUrl : 'pages/main.html', 
-                  controller : 'HomeController'})  
-    .when('/aboutus', { templateUrl : 'pages/aboutus.html', 
-               controller : 'AboutusController'})  
-    .when('/contactus', { templateUrl : 'pages/contactus.html', 
-                controller : 'ReviewsController'}) 
-    .when('/reviews', { templateUrl : 'pages/reviews.html', 
-                controller : 'ReviewsController'})
-    .otherwise({redirectTo: '/'}); 
+var app = angular.module("myApp", ["ngRoute"]);
+app.config(function ($routeProvider) {
+  $routeProvider
+    .when("/", { templateUrl: "pages/main.php", controller: "HomeController" })
+    .when("/aboutus", {
+      templateUrl: "pages/aboutus.php",
+      controller: "AboutusController",
+    })
+    .when("/contactus", {
+      templateUrl: "pages/contactus.php",
+      controller: "ReviewsController",
+    })
+    .when("/reviews", {
+      templateUrl: "pages/reviews.php",
+      controller: "ReviewsController",
+    })
+    .otherwise({ redirectTo: "/" });
 });
---
+++ pages/aboutUs.php
@@ -0,0 +1,215 @@
+<!-- about -->
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About Us Page</title>
+	<div class="navigateBack">
+		<a href="#!home"><h2>To go back to the home page, click here</h2></a>
+	</div>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+	<link rel="stylesheet" type="text/css" href="css/aboutUs.css">
+</head>
+
+<body>
+	<div class="title">
+		<h1 class="about-us-header">About Us</h1>
+		<p class="ryu-names">Name: Jacob Rokhvarg | SN: 500943379</p>
+		<p class="ryu-names">Name: Ralph Liton | SN: 500958086</p>
+		<p class="ryu-names">Georgz Simone Ilagan | SN: 50095818</p>
+	</div>
+	<div class="content">
+			<p>
+				<br>
+				<em>Our company will provide you with the highest quality furniture in Canada. We can supply you with anything,
+				you need, including: Chairs, Tables, Sofas, Drawers, Lamps, Chargers, Beds, Mattresses, Nightstands, Mirrors, Sinks, Bins</em>
+			</p>
+	</div>
+
+	<div class="contact">
+		<a href="#!contactus"><h3 style="text-align:center;">To contact us, click here!</h3> <br></a>
+	</div>
+
+	<hr style="border: 1px solid lightgrey;">
+
+	
+	
+<!-- Table design -->
+<div class="tableheader">
+		<br>
+		<h1 style="margin-bottom: 25px; margin-top: 25px;">Some of our products</h1>
+		<br>
+	</div>
+<div class="table-container">
+
+	<table style="width:60%;">
+	<tr>
+		<th><h2>Product<h2></th>
+		<th><h2>Image<h2></th>
+		<th><h2 style="margin-right:150px;">Description<h2></th>
+	</tr>
+	
+	<!-- Chairs -->
+	<tr>
+		<td><h3>Chairs: </h3></td>
+		<td><div id="chairImage"><img src="chair.JPG" alt="" border=1 height=250 width=250></img></div></td>
+		<td>
+		<div id="chairDescription">
+		<p>
+			<h4>
+				All of our chairs contain comfortable and ergonomic features, all while 
+				giving you a relaxed state of mind. Our chairs provide excelent back support 
+				and a very resilient frame, to ensure you always accomplish your goals.
+			</h4>
+		</p>
+		</div>
+		</td>
+	</tr>
+	
+	<!-- Tables -->
+	<tr>
+		<td><h3>Tables: </h3></td>
+		<td><div id="tableImage"><img src="table.JPG" alt="" border=1 height=250 width=250></img></div></td>
+		<td>
+		<div id="tableDescription">
+		<p>
+			<h4>
+				We care about the enviornment, and for this reason, all the materials in our tables
+				can be recycled. In addition to being eco-friendly, we offer tables with various
+				storage compartments, so you always stay organized, and on top of your work.
+			</h4>
+		</p>
+		</div>
+		</td>
+	</tr>
+	
+	<!-- Sofas -->
+	<tr>
+		<td><h3>Sofas: </h3></td>
+		<td><div id="sofaImage"><img src="sofa.JPG" alt="" border=1 height=250 width=250></img></div></td>
+		<td>
+		<div id="sofaDescription">
+		<p>
+			<h4>
+				Made with high quality materials and fabrics, we offer sofas to make sure you are always able to relax. 
+			</h4>
+		</p>
+		</div>
+		</td>
+	</tr>
+	
+	<!-- Drawers -->
+	<tr>
+		<td><h3>Drawers: </h3></td>
+		<td><div id="drawerImage"><img src="drawer.JPG" alt="" border=1 height=250 width=250></img></div></td>
+		<td>
+		<div id="drawerDescription">
+		<p>
+			<h4>
+				Made with high quality wood, ensuring your belongings are always kept neat and organized. 
+			</h4>
+		</p>
+		</div>
+		</td>
+	</tr>
+	
+	<!-- Lamps -->
+	<tr>
+		<td><h3>Lamps: </h3></td>
+		<td><div id="lampImage"><img src="lamp.JPG" alt="" border=1 height=250 width=250></img></div></td>
+		<td>
+		<div id="lampDescription">
+		<p>
+			<h4>
+				Made with enviornmentally friendly materials, we can provide with you with the most energy-efficient lamps on the market. 
+			</h4>
+		</p>
+		</div>
+		</td>
+	</tr>
+	
+	
+	<!-- Beds -->
+	<tr>
+		<td><h3>Beds: </h3></td>
+		<td><div id="bedImage"><img src="bed.JPG" alt="" border=1 height=250 width=250></img></div></td>
+		<td>
+		<div id="bedDescription">
+		<p>
+			<h4>
+				Made with a long lasting frame, to ensure you comfortable good nights sleep. 
+			</h4>
+		</p>
+		</div>
+		</td>
+	</tr>
+	
+	
+	<!-- Mattresses -->
+	<tr>
+		<td><h3>Mattresses: </h3></td>
+		<td><div id="mattressImage"><img src="mattress.JPG" alt="" border=1 height=250 width=250></img></div></td>
+		<td>
+		<div id="mattressDescription">
+		<p>
+			<h4>
+				Our mattresses are made with high quality materials to help you have a good, healthy sleep. 
+			</h4>
+		</p>
+		</div>
+		</td>
+	</tr>
+	
+	<!-- Nightstands -->
+	<tr>
+		<td><h3>Nightstands: </h3></td>
+		<td><div id="nightstandImage"><img src="nightstand.JPG" alt="" border=1 height=250 width=250></img></div></td>
+		<td>
+		<div id="nightstandDescription">
+		<p>
+			<h4>
+				Fits right in to the atmosphere of your bedroom, our nightstands are the most convenient storage units to have by your bed.
+			</h4>
+		</p>
+		</div>
+		</td>
+	</tr>
+	
+	
+	<!-- Mirrors -->
+	<tr>
+		<td><h3>Mirrors: </h3></td>
+		<td><div id="mirrorImage"><img src="mirror.JPG" alt="" border=1 height=250 width=250></img></div></td>
+		<td>
+		<div id="mirrorDescription">
+		<p>
+			<h4>
+				Made with glass that will last a lifetime. 
+			</h4>
+		</p>
+		</div>
+		</td>
+	</tr>
+	
+	
+	<!-- Sinks -->
+	<tr>
+		<td><h3>Sinks: </h3></td>
+		<td><div id="sinkImage"><img src="sink.JPG" alt="" border=1 height=250 width=250></img></div></td>
+		<td>
+		<div id="sinkDescription">
+		<p>
+			<h4>
+				Our sinks are made with high quality aluminum to reduce the possibiliby of rust, therefore ensuring a long lasting product. 
+			</h4>
+		</p>
+		</div>
+		</td>
+	</tr>
+	</table>
+
+</div>
+
+</body>
+</html>
