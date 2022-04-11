043925660626e03c35b1a705ee74a8893778ed8c
Ralph Liton <rj_liton@hotmail.com>
2022-04-11 19:04:22 -0400

    drag & drop to cart

--- js/controllers.js
+++ js/controllers.js
@@ -1,159 +1,167 @@
-app.controller('chairController', function ($scope, $http) {
-  $scope.type = 'CHA';
+app.controller("chairController", function ($scope, $http) {
+  $scope.type = "CHA";
 
   var data = JSON.stringify({ type: $scope.type });
 
-  $http.post("php/catalogue.php", data)
+  $http
+    .post("php/catalogue.php", data)
     .then(function successCallback(response) {
       let res = JSON.stringify(response.data.items[0]);
       let ress = JSON.parse(res);
       $scope.items = ress;
-    })
+    });
 });
 
-app.controller('tableController', function ($scope, $http) {
-  $scope.type = 'TAB';
+app.controller("tableController", function ($scope, $http) {
+  $scope.type = "TAB";
 
   var data = JSON.stringify({ type: $scope.type });
 
-  $http.post("php/catalogue.php", data)
+  $http
+    .post("php/catalogue.php", data)
     .then(function successCallback(response) {
       let res = JSON.stringify(response.data.items[0]);
       let ress = JSON.parse(res);
       $scope.items = ress;
-    })
+    });
 });
 
-app.controller('sofaController', function ($scope, $http) {
-  $scope.type = 'SOF';
+app.controller("sofaController", function ($scope, $http) {
+  $scope.type = "SOF";
 
   var data = JSON.stringify({ type: $scope.type });
 
-  $http.post("php/catalogue.php", data)
+  $http
+    .post("php/catalogue.php", data)
     .then(function successCallback(response) {
       let res = JSON.stringify(response.data.items[0]);
       let ress = JSON.parse(res);
       $scope.items = ress;
-    })
+    });
 });
 
-
-app.controller('drawerController', function ($scope, $http) {
-  $scope.type = 'DRA';
+app.controller("drawerController", function ($scope, $http) {
+  $scope.type = "DRA";
 
   var data = JSON.stringify({ type: $scope.type });
 
-  $http.post("php/catalogue.php", data)
+  $http
+    .post("php/catalogue.php", data)
     .then(function successCallback(response) {
       let res = JSON.stringify(response.data.items[0]);
       let ress = JSON.parse(res);
       $scope.items = ress;
-    })
+    });
 });
 
-app.controller('lampController', function ($scope, $http) {
-  $scope.type = 'LAM';
+app.controller("lampController", function ($scope, $http) {
+  $scope.type = "LAM";
 
   var data = JSON.stringify({ type: $scope.type });
 
-  $http.post("php/catalogue.php", data)
+  $http
+    .post("php/catalogue.php", data)
     .then(function successCallback(response) {
       let res = JSON.stringify(response.data.items[0]);
       let ress = JSON.parse(res);
       $scope.items = ress;
-    })
+    });
 });
 
-app.controller('chargerController', function ($scope, $http) {
-  $scope.type = 'CHG';
+app.controller("chargerController", function ($scope, $http) {
+  $scope.type = "CHG";
 
   var data = JSON.stringify({ type: $scope.type });
 
-  $http.post("php/catalogue.php", data)
+  $http
+    .post("php/catalogue.php", data)
     .then(function successCallback(response) {
       let res = JSON.stringify(response.data.items[0]);
       let ress = JSON.parse(res);
       $scope.items = ress;
-    })
+    });
 });
 
-
-app.controller('bedController', function ($scope, $http) {
-  $scope.type = 'BED';
+app.controller("bedController", function ($scope, $http) {
+  $scope.type = "BED";
 
   var data = JSON.stringify({ type: $scope.type });
 
-  $http.post("php/catalogue.php", data)
+  $http
+    .post("php/catalogue.php", data)
     .then(function successCallback(response) {
       let res = JSON.stringify(response.data.items[0]);
       let ress = JSON.parse(res);
       $scope.items = ress;
-    })
+    });
 });
 
-app.controller('mattressController', function ($scope, $http) {
-  $scope.type = 'MAT';
+app.controller("mattressController", function ($scope, $http) {
+  $scope.type = "MAT";
 
   var data = JSON.stringify({ type: $scope.type });
 
-  $http.post("php/catalogue.php", data)
+  $http
+    .post("php/catalogue.php", data)
     .then(function successCallback(response) {
       let res = JSON.stringify(response.data.items[0]);
       let ress = JSON.parse(res);
       $scope.items = ress;
-    })
+    });
 });
 
-app.controller('nightstandController', function ($scope, $http) {
-  $scope.type = 'NST';
+app.controller("nightstandController", function ($scope, $http) {
+  $scope.type = "NST";
 
   var data = JSON.stringify({ type: $scope.type });
 
-  $http.post("php/catalogue.php", data)
+  $http
+    .post("php/catalogue.php", data)
     .then(function successCallback(response) {
       let res = JSON.stringify(response.data.items[0]);
       let ress = JSON.parse(res);
       $scope.items = ress;
-    })
+    });
 });
 
-
-
-app.controller('mirrorController', function ($scope, $http) {
-  $scope.type = 'MIR';
+app.controller("mirrorController", function ($scope, $http) {
+  $scope.type = "MIR";
 
   var data = JSON.stringify({ type: $scope.type });
 
-  $http.post("php/catalogue.php", data)
+  $http
+    .post("php/catalogue.php", data)
     .then(function successCallback(response) {
       let res = JSON.stringify(response.data.items[0]);
       let ress = JSON.parse(res);
       $scope.items = ress;
-    })
+    });
 });
 
-app.controller('sinkController', function ($scope, $http) {
-  $scope.type = 'SIN';
+app.controller("sinkController", function ($scope, $http) {
+  $scope.type = "SIN";
 
   var data = JSON.stringify({ type: $scope.type });
 
-  $http.post("php/catalogue.php", data)
+  $http
+    .post("php/catalogue.php", data)
     .then(function successCallback(response) {
       let res = JSON.stringify(response.data.items[0]);
       let ress = JSON.parse(res);
       $scope.items = ress;
-    })
+    });
 });
 
-app.controller('binController', function ($scope, $http) {
-  $scope.type = 'BIN';
+app.controller("binController", function ($scope, $http) {
+  $scope.type = "BIN";
 
   var data = JSON.stringify({ type: $scope.type });
 
-  $http.post("php/catalogue.php", data)
+  $http
+    .post("php/catalogue.php", data)
     .then(function successCallback(response) {
       let res = JSON.stringify(response.data.items[0]);
       let ress = JSON.parse(res);
       $scope.items = ress;
-    })
+    });
 });
--- js/routing.js
+++ js/routing.js
@@ -1,30 +1,24 @@
 var app = angular.module("myApp", ["ngRoute"]);
 app.config(function ($routeProvider) {
   $routeProvider
-    .when("/", { templateUrl: "pages/main.html", })
+    .when("/", { templateUrl: "pages/main.html" })
     .when("/aboutus", {
       templateUrl: "pages/aboutus.html",
-
     })
     .when("/contactus", {
       templateUrl: "pages/contactus.html",
-
     })
     .when("/reviews", {
       templateUrl: "pages/reviews.html",
-
     })
     .when("/signUp", {
       templateUrl: "pages/signUp.html",
-
     })
     .when("/login", {
       templateUrl: "pages/login.html",
-
     })
     .when("/logOut", {
       templateUrl: "pages/logOut.html",
-
     })
     .when("/chairs", {
       templateUrl: "pages/catalogue.html",
